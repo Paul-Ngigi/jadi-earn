fd48d4e3d63c02c47a7aabedad664cb20ef56c93
WilsonKinyua <wilsonkinyuam@gmail.com>
2021-04-21 02:57:36 +0300

    starting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    DashboardHomeComponent
   ],
   imports: [
     BrowserModule,
